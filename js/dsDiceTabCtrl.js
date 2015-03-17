48f360b3330524ddac3fa789c7dca0be89205336
Mike Hasko <psychopez@gmail.com>
2015-03-16 22:31:25 -0400

    guiDice tracked fully by the dsDiceService.  Add, remove, roll, and clear.

---
+++ js/dsDiceTabCtrl.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var dsApp = angular.module('dicestreamApp');
+
+dsApp.controller('diceTabCtrl', ['$scope', 'diceService', function ($scope, diceService) {
+    $scope.roll = function() {
+        diceService.rollDice();
+    };
+    
+    $scope.clear = function() {
+        diceService.clearDice();
+    };
+    
+    $scope.getRolledDice = function() {
+        return diceService.getRolledDiceArray();
+    }
+}]);
+
+//return dsApp;
