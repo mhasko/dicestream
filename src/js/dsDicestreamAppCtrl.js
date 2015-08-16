41050314150a87ead93a05894d7ee8b1a5cfbdf5
Mike Hasko <psychopez@gmail.com>
2015-08-16 14:05:03 -0400

    Change file dir, use grunt-copy for dev/prod
    Use grunt to help build dev and prod code.  Required changing the file
    structure to have a src directory, which can allow prod and dev builds.

--- src/js/dsDicestreamAppCtrl.js
+++ src/js/dsDicestreamAppCtrl.js
@@ -7,7 +7,8 @@ dsApp.config(function($sceDelegateProvider){
 });
 
 dsApp.constant('config', {
-    filePrefix: 'https://dl.dropbox.com/u/1177409/dicestream/',
+    //filePrefix: 'https://dl.dropbox.com/u/1177409/dicestream/public/src',
+    filePrefix: '%rootPath%',
 //    file-prefix: 'https://s3.amazonaws.com/dicestream/',
     imgroot: 'https://s3.amazonaws.com/dicestream/images/'
 });
